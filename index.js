9c4c9cc882a823c3a7013279f3addc8694e7b4b2
Ahmed Fawzi <np1265448@gmail.com>
2023-05-17 18:01:43 +0300

    update

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ require("dotenv").config();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use("/favicon.ico", express.static("public/img/favicon.ico"));
 app.use(
   session({
     secret: "1234567890abcdefghijklmnopqrstuvwxyz",
@@ -27,7 +28,6 @@ const corsOptions = {
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cors(corsOptions));
-
 //? }
 //!const routes {
 const ProductRoute = require("./routes/Product.route");
