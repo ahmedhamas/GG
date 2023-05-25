5c454c254affda664a49f2b639e25264872e86a9
Ahmed Fawzi <np1265448@gmail.com>
2023-05-25 23:02:25 +0300

    paymob

--- public/js/cart.js
+++ public/js/cart.js
@@ -38,7 +38,9 @@ function calContainer() {
       auth.innerHTML = `<p> من فضلك سجل الدخول لمواصلة <a href='/user/info/login'>الشراء</a> </p>`;
     } else {
       auth.innerHTML = ` <p class='checkoutBtn'><a href='/pay/info/cash_on_delivery'>الدفع عند الاستلام</a></p>
-        <p class='checkoutBtn'><a href='/pay/info/credit_card'>ببطاقة ائتمان</a></p>`;
+        <p class='checkoutBtn'><a href='/pay/info/credit_card'>ببطاقة ائتمان</a></p>
+        <button class='checkoutBtn' onclick="Paymob()">paymob</button>
+        `;
     }
   } else {
     const countContainer = document.getElementById("countContainer");
@@ -124,4 +126,85 @@ function dicQuantity(productId) {
     }
   }
   localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+const apiKey =
+  "ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpiR0Z6Y3lJNklrMWxjbU5vWVc1MElpd2ljSEp2Wm1sc1pWOXdheUk2TnprNE5EVTFMQ0p1WVcxbElqb2lhVzVwZEdsaGJDSjkuZzd1YzV2RlBqVXFJMUFmaHVjUnZ1YXdqU0EzSm9oVkZ3OTFZTlN1ZWhIZWNrb1ZHbktzMDZRQVIzdUNWaU5oTGxDRGpEblVhMDJoRGczMVVpSnEwLUE=";
+
+async function Paymob() {
+  let data = {
+    api_key: apiKey,
+  };
+  let request = await fetch("https://accept.paymob.com/api/auth/tokens", {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+  let res = await request.json();
+  let token = res.token;
+
+  secStep(token);
+}
+async function secStep(token) {
+  let data = {
+    auth_token: token,
+    delivery_needed: "false",
+    amount_cents: "100",
+    currency: "EGP",
+    items: [],
+  };
+  let req = await fetch("https://accept.paymob.com/api/ecommerce/orders", {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  let res = await req.json();
+
+  thirdStep(token, res.id);
+}
+
+async function thirdStep(token, id) {
+  let data = {
+    auth_token: token,
+    amount_cents: "100",
+    expiration: 3600,
+    order_id: id,
+    billing_data: {
+      apartment: "803",
+      email: "claudette09@exa.com",
+      floor: "42",
+      first_name: "Clifford",
+      street: "Ethan Land",
+      building: "8028",
+      phone_number: "+86(8)9135210487",
+      shipping_method: "PKG",
+      postal_code: "01898",
+      city: "Jaskolskiburgh",
+      country: "CR",
+      last_name: "Nicolas",
+      state: "Utah",
+    },
+    currency: "EGP",
+    integration_id: 3837851,
+  };
+
+  let req = await fetch(
+    "https://accept.paymob.com/api/acceptance/payment_keys",
+    {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    }
+  );
+
+  let res = await req.json();
+
+  let TheToken = res.token;
+
+  cardPayment(TheToken);
+}
+async function cardPayment(TheToken) {
+  iframeUrl = `https://accept.paymob.com/api/acceptance/iframes/761516?payment_token=${TheToken}`;
+  location.replace(iframeUrl);
 }
